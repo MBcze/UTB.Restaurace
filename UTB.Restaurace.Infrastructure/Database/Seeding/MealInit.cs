589e0df5f0893a0254aeae5e66fb0ac2f6370491
m_andrysek <m_andrysek@utb.cz>
2024-12-11 10:13:51 +0100

    Přidání obrázků a změna prvotního nahrání do databáze

--- UTB.Restaurace.Infrastructure/Database/Seeding/MealInit.cs
+++ UTB.Restaurace.Infrastructure/Database/Seeding/MealInit.cs
@@ -20,7 +20,7 @@ namespace UTB.Restaurace.Infrastructure.Database.Seeding
                 Name = "Kuřecí řízek v bylinkové omáčce",
                 Description = "Křupavý kuřecí řízek podávaný s jemnou bylinkovou omáčkou a čerstvým salátem.",
                 Price = 165.00,
-                ImageSrc = "/img/kureci_rizek.jpg",
+                ImageSrc = "/img/chickenSchnitzel.jpg",
                 Category = "hlavní jídlo",
                 Available = true
             });
@@ -31,7 +31,7 @@ namespace UTB.Restaurace.Infrastructure.Database.Seeding
                 Name = "Flank Steak",
                 Description = "Šťavnatý flank steak, grilovaný na střední propečení, podávaný s bylinkovým máslem.",
                 Price = 250.00,
-                ImageSrc = "/img/flank_steak.jpg",
+                ImageSrc = "/img/flankSteak.jpg",
                 Category = "hlavní jídlo",
                 Available = false
             });
@@ -42,10 +42,88 @@ namespace UTB.Restaurace.Infrastructure.Database.Seeding
                 Name = "Vepřové s knedlíkem a zelím",
                 Description = "Tradiční české jídlo s pečeným vepřovým masem, knedlíky a zelím.",
                 Price = 180.00,
-                ImageSrc = "/img/vepro_knedlo_zelo.jpg",
+                ImageSrc = "/img/veproKnedloZelo.jpg",
                 Category = "hlavní jídlo",
                 Available = true
             });
+
+            meals.Add(new Meal
+            {
+                Id = 4,
+                Name = "Hovězí Burger",
+                Description = "Šťavnatý hovězí burger s čerstvou zeleninou a domácí omáčkou.",
+                Price = 200.00,
+                ImageSrc = "/img/burger.jpg",
+                Category = "hlavní jídlo",
+                Available = true
+            });
+
+            meals.Add(new Meal
+            {
+                Id = 5,
+                Name = "Pepperonni pizza",
+                Description = "Italská pizza s pikantními plátky pepperoni a sýrem mozzarella.",
+                Price = 220.00,
+                ImageSrc = "/img/pizza.jpg",
+                Category = "hlavní jídlo",
+                Available = true
+            });
+
+            meals.Add(new Meal
+            {
+                Id = 6,
+                Name = "Smažený sýr",
+                Description = "Oblíbený smažený sýr podávaný s hranolky/kroketami a tatarskou omáčkou.",
+                Price = 150.00,
+                ImageSrc = "/img/smazenySyr.jpg",
+                Category = "hlavní jídlo",
+                Available = true
+            });
+
+            meals.Add(new Meal
+            {
+                Id = 7,
+                Name = "Rajčatová polévka",
+                Description = "Krémová rajčatová polévka zdobená čerstvou bazalkou a parmazánem.",
+                Price = 95.00,
+                ImageSrc = "/img/tomatoSoup.jpg",
+                Category = "polévka",
+                Available = true
+            });
+
+            meals.Add(new Meal
+            {
+                Id = 8,
+                Name = "Coca Cola",
+                Description = "Osvěžující nealkoholický nápoj s originální recepturou.",
+                Price = 35.00,
+                ImageSrc = "/img/cocaCola.jpg",
+                Category = "nápoj",
+                Available = true
+            });
+
+            meals.Add(new Meal
+            {
+                Id = 9,
+                Name = "Pepsi",
+                Description = "Populární nealkoholický nápoj s jedinečnou chutí.",
+                Price = 35.00,
+                ImageSrc = "/img/pepsi.jpg",
+                Category = "nápoj",
+                Available = true
+            });
+
+            meals.Add(new Meal
+            {
+                Id = 10,
+                Name = "Staropramen",
+                Description = "Klasické české pivo s vyváženou chutí a jemnou hořkostí.",
+                Price = 45.00,
+                ImageSrc = "/img/staropramen.jpg",
+                Category = "nápoj",
+                Available = true
+            });
+
 
             return meals;
         }
