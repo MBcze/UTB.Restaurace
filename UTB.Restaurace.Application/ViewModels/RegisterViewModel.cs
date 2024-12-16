85d0491b31d4be40559263c00a0de1360b20aa6c
MBcze <m_bradavka@utb.cz>
2024-12-16 20:17:41 +0100

    ViewModels přihlášovací a registrační formulář

---
+++ UTB.Restaurace.Application/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTB.Restaurace.Application.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string? UserName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Phone]
+        public string? PhoneNumber { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        [Required]
+        public string? PasswordHash { get; set; }
+        [Required]
+        [Compare(nameof(PasswordHash), ErrorMessage = "Passwords don't match!")]
+        public string? RepeatedPassword { get; set; }
+    }
+}
